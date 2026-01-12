c6a6e9f8c1f00cbf28c272d589d91b8d46c05157
RT STORE <emanuelima1802@gmail.com>
2026-01-12 03:38:19 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -16,15 +16,7 @@ const io = socketIo(server);
 
 // Configurações de segurança
 app.use(helmet({
-  contentSecurityPolicy: {
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'"],
-      styleSrc: ["'self'", "'unsafe-inline'"],
-      imgSrc: ["'self'", "data:", "https:"],
-      connectSrc: ["'self'", "ws:", "wss:"]
-    }
-  }
+  contentSecurityPolicy: false
 }));
 
 app.use(cors());
