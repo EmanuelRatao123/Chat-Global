b275e91a903207c6e239a708bf685aa2b71bb046
RT STORE <emanuelima1802@gmail.com>
2026-01-12 03:36:21 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(helmet({
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+
 
 // Rate limiting
 const limiter = rateLimit({
@@ -401,6 +401,14 @@ io.on('connection', (socket) => {
 });
 
 // Servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Catch all para SPA
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
